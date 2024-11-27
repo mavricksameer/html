c241b0948375ea00527efa9820bc3aab31a3269b
mavricksameer <jrsameer108@gmail.com>
2024-11-27 18:36:11 +0500

    pratice JS

--- pratice.js
+++ pratice.js
@@ -8,5 +8,8 @@ const rollnumber = 20
 console.log(`my name is ${name} and my roll number is ${rollnumber}`);
 const gameName = new String ('Sameer')
 console.log( gameName.charAt(2));
+console.log( gameName.indexOf("r"));
+const url ="https://github.com/mavricksameer"
+console.log(url.replace ("/","-"));
 
 
