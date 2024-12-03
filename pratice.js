a5e3443a617f308b48ae15d6e9f88e8afe30f635
mavricksameer <jrsameer108@gmail.com>
2024-12-03 16:06:21 +0500

    pratice

--- pratice.js
+++ pratice.js
@@ -6,29 +6,30 @@ const name = "sameer"
 const rollnumber = 20
 
 console.log(`my name is ${name} and my roll number is ${rollnumber}`);
-const gameName = new String ('Sameer')
-console.log( gameName.charAt(2));
-console.log( gameName.indexOf("r"));
-const url ="https://github.com/mavricksameer"
-console.log(url.replace ("/","-"));
+const gameName = new String('Sameer')
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("r"));
+const url = "https://github.com/mavricksameer"
+console.log(url.replace("/", "-"));
 const score = 400
 console.log(score);
 console.log(Math);
-console.log(Math.abs (-4));
-console.log(Math.round (4.6));
-console.log(Math.ceil (4.2));
-console.log(Math.floor (4.2));
-console.log(Math.min (4,2,1,3));
-console.log(Math.max (9,2,1,3));
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.min(4, 2, 1, 3));
+console.log(Math.max(9, 2, 1, 3));
 
 // date
 // let myDate = new Date()
 // console.log(myDate.toLocaleString());
 // let myCreateDate = new Date (2022, 0, 09)
 // console.log(myCreateDate.toDateString());
-let myDate = new Date (2022,2,23)
+let myDate = new Date(2022, 2, 23)
 console.log(myDate.getMonth());
 meme
+shaheen
 
 
 
